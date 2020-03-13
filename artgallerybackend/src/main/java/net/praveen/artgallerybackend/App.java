1e978ecda7a8edc849034f6a0802b24a96588740
Praveen Panikar <praveen222animator@gmail.com>
2020-03-13 12:37:22 +0530

    my project setup completed successfully

---
+++ artgallerybackend/src/main/java/net/praveen/artgallerybackend/App.java
@@ -0,0 +1,13 @@
+package net.praveen.artgallerybackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
